ef59f82a3a9b44b0d6edea46942bce0a58b96984
Luca Manniti <mannitello@gmail.com>
2014-05-27 15:09:33 +0100

    le_migrate_all script

--- le_migrate_tags/le_migrate_tags.py
+++ le_migrate_tags/le_migrate_tags.py
@@ -20,10 +20,10 @@ class LeClient(object):
         req = urllib2.Request('https://api.logentries.com/v2/hooks')
         req.add_header('Content-Type', 'application/json') #This is what I expect to receive (Json)
         response = urllib2.urlopen(req, json.dumps(request)) #Ask Peter/Stephen to explain
-        hook = json.loads(response.read())
-       # print '\n\nHOOKS\n',hook['hooks']
-        return hook
-       
+        hooks = json.loads(response.read())
+        le=LeClient(self.account_key)
+        return hooks['hooks']
+               
 
     def get_tags(self):
         request = {
@@ -35,8 +35,10 @@ class LeClient(object):
         req.add_header('Content-Type', 'application/json')
         response = urllib2.urlopen(req, json.dumps(request))
         tags = json.loads(response.read())
-        # print '\n\nTAGS\n',tags
-        return tags
+        le=LeClient(self.account_key)
+        return tags['tags']
+    
+ 
 
     def get_actions(self):
         request = {
@@ -48,8 +50,8 @@ class LeClient(object):
         req.add_header('Content-Type', 'application/json')
         response = urllib2.urlopen(req, json.dumps(request))
         actions = json.loads(response.read())
-    #    print '\n\nACTIONS\n',actions
-        return actions
+        return actions['actions']
+
 
     def create_json_object(self):
         json_object_app={}
@@ -60,44 +62,86 @@ class LeClient(object):
 
 
 
-        
+    def upload_hook(self,hook,actions,tags):
+        request = {
+            "name": hook['name'],
+            "triggers": hook['triggers'],
+            "sources": hook['sources'],
+            "groups": [],
+            "actions": [],
+            "request": "create",
+            "account": self.account_key,
+            "acl": self.account_key
+        }
+        req='https://api.logentries.com/v2/hooks'
+        headers={'Content-Type': 'application/json'}
+        for Id in hook['actions']:
+            for act in actions:
+                if act['id']==Id:
+                    request['actions'].append(self.upload_actions(act,tags))
+        r = requests.post(req,data = json.dumps(request),headers = headers)
+
+    def upload_actions(self,actions,tags):
+        request = {
+            "rate_count": actions['rate_count'],
+            "rate_range": actions['rate_range'],
+            "limit_count": actions['limit_count'],
+            "limit_range": actions['limit_range'],
+            "schedule": [],
+            "type": actions['type'],
+            "args": actions['args'],
+            "request": "create",
+            "account": self.account_key,
+            "acl": self.account_key
+            }
+        req='https://api.logentries.com/v2/actions'
+        headers={'Content-Type': 'application/json'}
+        if actions['type'] == 'tagit':
+             for tag in tags:
+                if tag['sn']==int(actions['args']['sn']):
+                    actions['args']['sn']=self.upload_tags(tag)
+
+        r = requests.post(req,data = json.dumps(request),headers = headers)
+        actions=json.loads(r.text)
+        return actions['id']
+
+    def upload_tags(self,tags):
+        request = {
+            "name": tags['name'],
+            "title": tags['title'],
+            "description": tags['description'],
+            "appearance": tags['appearance'],
+            "request": "create",
+            "account": self.account_key,
+            "acl": self.account_key
+            }
+        req='https://api.logentries.com/v2/tags'
+        headers={'Content-Type': 'application/json'}
+        r = requests.post(req,data = json.dumps(request),headers = headers)
+        response=json.loads(r.text)
+        return response['sn']
+
+  
 def main():
     ap=argparse.ArgumentParser()
     ap.add_argument('account_key',help='If you want to export tags put the account key of the account you want to take tags from \n\n If you want to import tags on your account put your account key')
     ap.add_argument('action',choices=['import','export'],help='Choose export if you want to export the tags from an account\n\n Choose import if you want to import someone else tags into your account')
     ap.add_argument('filename',help='Choose the file name where you want to export/import the tags')
     args=ap.parse_args(sys.argv[1:])
-    print args
     le=LeClient(args.account_key)
     if args.action=='export':
         file = open(args.filename,'w+')
         file.write(le.create_json_object())
     else:
-        request = {
-            'request': 'list',
-            'account': args.account_key,
-            'acl': args.account_key
-        }
-        json_data=open(args.filename)
-        json_big_object=json.load(json_data)
-        Hooks=json.dumps(json_big_object['Hooks'])
-        Tags=json.dumps(json_big_object['Tags'])
-        Actions=json.dumps(json_big_object['Actions'])
-        req1='https://api.logentries.com/v2/hooks'
-        headers={'Content-Type': 'application/json'}
-        r1 = requests.post(req1,Hooks)
-        print r1.status_code
-        print r1.content
-        req2='https://api.logentries.com/v2/tags'
-        r2 = requests.post(req2,Tags)
-        print r2
-        print r2.status_code
-        print r2.content
-        req3='https://api.logentries.com/v2/actions'
-        r3 = requests.post(req3,Actions)
-
-       
-
+        file = open(args.filename,'r')
+        object_all = json.loads(file.read())
+        Hooks = object_all['Hooks']
+        Actions = object_all['Actions']
+        Tags = object_all['Tags']
+        for hook in Hooks:
+            le.upload_hook(hook,Actions,Tags)
+        
+  
 if __name__ == '__main__':
     main()
 
